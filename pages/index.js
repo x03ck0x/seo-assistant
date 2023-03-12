60be4f23b63bc012ce0cbc09d8d3d301c3bed997
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-13 00:39:56 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,3 @@
 import seo from './seo';
-import pets from './pets';
-import react from "react";
-import Head from "next/head";
-import ReactDOM from 'react-dom';
 
-//export default seo
-
-const AppLinks = () => {
-    return (
-      <ul>
-        <li><a href="./pets">App</a></li>
-        <li><a href="./seo">Seo</a></li>
-        {/* add more links for other app files here */}
-      </ul>
-    )
-  }
-  
+export default seo
