45da2a831213d12889f5a8f0cf185af995218720
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-13 00:11:07 +0100

    sf

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
 import seo from './seo';
+import pets from './pets';
 
-export default seo
+//export default seo
+
+const AppLinks = () => {
+    return (
+      <ul>
+        <li><a href="./pets">App</a></li>
+        <li><a href="./seo">Seo</a></li>
+        {/* add more links for other app files here */}
+      </ul>
+    )
+  }
+  
+  ReactDOM.render(
+    <>
+      <Seo />
+      <AppLinks />
+      <App />
+    </>,
+    document.getElementById('root')
+  );
