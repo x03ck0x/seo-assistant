5e807bd0168f6978fc19e0a580a9b7b138d40146
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-13 01:55:24 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
 import seo from './seo';
+import Link from "next/link";
 
-export default seo
+//export default seo
+
+export default () => (
+  <div>
+
+    <style jsx global>{`
+      body {
+        background-color: red;
+      }
+    `}</style>
+
+    Hello, One!
+    <Link href="/seo">
+      <a>Go to two</a>
+    </Link>
+  </div>
+);
