1994ce5b1a671bb3a315b0db7d63feb8aeea26c6
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-14 02:05:57 +0100

    s

---
+++ pages/alternative.js
@@ -0,0 +1,97 @@
+import react from "react";
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [text, setText] = useState("");
+  const [language, setLanguage] = useState("French");
+
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState();
+  
+  async function onSubmit(event) {
+    event.preventDefault();
+    
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+
+      const response = await fetch('/api/generate-alt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text, language }),
+      });
+      const data = await response.json();
+      setResult(data.result.replaceAll('\n', '<br />'));
+
+    } catch (e) {
+      alert('failed to submit, try again')
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    
+    <div className="container">
+      <Head>
+        <title>Seo Text Alternative</title>
+        <link rel="icon" href="/skull.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/skull.png" className={styles.icon} />
+        <h3>Seo Text Alternative</h3>
+        <form onSubmit={onSubmit}>
+          <label>Text</label>
+          <textarea
+            name="text"
+            placeholder="Enter the text here"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          <label>language</label>
+          <select
+            name="language"
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+          >
+            <option value="French">French</option>
+            <option value="English">English</option>
+            <option value="Dutch">Dutch</option>
+          </select>
+
+          <input type="submit" value="Generate" />          
+        </form>
+
+        {loading && (
+          <div>
+            <h4>Working on the best SEO</h4>
+            { <img src="/css-load.gif" className={styles.loading} /> }
+
+          </div>
+        )}
+        {result && (
+        <div className={styles.result} 
+        dangerouslySetInnerHTML={{ __html: result }}
+        />
+        )}
+      </main>
+
+      <style jsx global>{`
+        body {
+          background-color: #fff;
+        }
+      `}</style>
+
+    </div>
+  );
+}
+
+
---
+++ pages/api/generate-alt.js
@@ -0,0 +1,33 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+    const {text} = req.body;
+    const prompt = generatePrompt(text, language);
+
+    console.log(prompt);
+
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.6,
+      max_tokens: 400,
+    });
+
+    res.status(200).json({ result: completion.data.choices[0].text });  
+}
+
+function generatePrompt(text, language) {
+  
+  //return `Generate a high-quality Meta Description in for this keywords: ${keywords}$ , for the entreprise name use this ${name}$ , use ${services}$ as services they provide, use this address: ${address}$ , as address or city.`
+  return `
+  You are an expert in SEO speaking fluent ${language}$. For the following text prompt 
+  -- [PROMPT] ${text} ,
+  please write an alternative article almost identical to the text, including line breaks. 
+  Everything must be in language ${language}.
+  `
+}
