5b11d661c5d8055a78c97fce95a3c1f39951f3c1
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-14 01:45:20 +0100

    s

--- pages/api/generate-seo.js
+++ pages/api/generate-seo.js
@@ -12,7 +12,7 @@ export default async function (req, res) {
     console.log(prompt);
 
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: prompt,
       temperature: 0.6,
       max_tokens: 200,
