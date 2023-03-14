c3e9ae3b2ce6ce802809a1f63b4c479101e591a3
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-14 02:54:30 +0100

    d

--- pages/alternative.js
+++ pages/alternative.js
@@ -51,7 +51,7 @@ export default function Home() {
         <form onSubmit={onSubmit}>
           <label>Text</label>
           <input
-            type="textarea"
+            type="text"
             name="text"
             placeholder="Enter the text here"
             value={text}
