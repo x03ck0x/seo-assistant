8854cd33e76b0bbe9f5b86ea5d6b59df1e40a787
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-26 23:38:00 +0200

    moor4

--- pages/api/generate-img.js
+++ pages/api/generate-img.js
@@ -1,21 +1,32 @@
 import { Configuration, OpenAIApi } from 'openai';
+import axios from 'axios';
 
-const configuration = new Configuration({
+/* const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
+ */
 
 const generateImage = async () => {
   try {
-    console.log(openai)
-    const response = await openai.createImage({
-      prompt: 'a white siamese cat',
-      n: 1,
-      size: '1024x1024',
-    });
+    const response = await axios.post(
+      'https://api.openai.com/v1/images/generations',
+      {
+        prompt: 'a white siamese cat',
+        n: 1,
+        size: '1024x1024',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'X-User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        },
+      }
+    );
 
-    const imageUrl = response.data[0].url;
+    const imageUrl = response.data.data[0].url;
     console.log(imageUrl);
     return imageUrl;
   } catch (error) {
