fe4f8a5c31c0e73d2a2eefebcfa1ef80ef02f1f7
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-27 00:10:30 +0200

    moor4

--- pages/api/generate-img.js
+++ pages/api/generate-img.js
@@ -2,35 +2,27 @@ const express = require('express');
 const { Configuration, OpenAIApi } = require('openai');
 const cors = require('cors');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-app.use(express.json());
-
-app.post('/api/generate-image', async (req, res) => {
+const generateImage = async () => {
   try {
-    const { prompt, n, size } = req.body;
-
-    const response = await openai.image.create({
-      prompt,
-      n,
-      size,
+    const response = await openai.imageBeta.create({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
     });
 
     const imageUrl = response.data[0].url;
-
-    res.json({ imageUrl });
+    console.log(imageUrl);
+    return imageUrl;
   } catch (error) {
-    console.error('Failed to generate image:', error);
-    res.status(500).json({ error: 'Failed to generate image' });
+    console.error("Failed to generate image:", error);
   }
-});
+};
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+export default generateImage;
+
