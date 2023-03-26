f34f0ed969f2061c23ffe8384eb33973dab54e94
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-26 23:27:38 +0200

    moor4

--- pages/img.js
+++ pages/img.js
@@ -14,6 +14,15 @@ const ImageGenerator = () => {
     <div>
       <button onClick={handleGenerateImage}>Generate Image</button>
       {imageUrl && <img src={imageUrl} alt="Generated Image" />}
+      <style jsx global>{`
+        body {
+          background-color: #212129;
+          color: #fff !important;
+        }
+        p, h1, h2, h3, h4, h5, h6 {
+          color: #fff !important;
+        }
+      `}</style>
     </div>
   );
 };
