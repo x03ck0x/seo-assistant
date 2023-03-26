9ff1fdb6ab365b32e1cadef0cf794562ceecd1e9
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-27 00:07:12 +0200

    moor4

--- pages/api/generate-img.js
+++ pages/api/generate-img.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Configuration, OpenAIApi } = require('openai');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
