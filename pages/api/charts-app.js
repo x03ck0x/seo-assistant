49577219d3fc224ab244bc44ec4cc1c83dfaa0f8
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-27 01:51:38 +0200

    moor4

---
+++ pages/api/charts-app.js
@@ -0,0 +1,44 @@
+// components/HighchartsPage.js
+import React, { useEffect, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const HighchartsPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.stlouisfed.org/fred/series/observations?series_id=GNPCA&api_key=3470b12101b9496a3ef4c864044a0692&file_type=json')
+      .then(response => response.json())
+      .then(json => setData(json.observations))
+      .catch(error => console.error(error));
+  }, []);
+
+  const options = {
+    title: {
+      text: 'US Real Gross Domestic Product'
+    },
+    series: [{
+      name: 'GDP',
+      data: data.map(datum => [new Date(datum.date).getTime(), parseFloat(datum.value)])
+    }],
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Year'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Billions of Dollars'
+      }
+    }
+  };
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+};
+
+export default HighchartsPage;
--- pages/charts.js
+++ pages/charts.js
@@ -1,43 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+// pages/index.js
+import React from 'react';
+import HighchartsPage from './api/charts-app';
 
-const HighchartsPage = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.stlouisfed.org/fred/series/observations?series_id=GNPCA&api_key=3470b12101b9496a3ef4c864044a0692&file_type=json')
-      .then(response => response.json())
-      .then(json => setData(json.observations))
-      .catch(error => console.error(error));
-  }, []);
-
-  const options = {
-    title: {
-      text: 'US Real Gross Domestic Product'
-    },
-    series: [{
-      name: 'GDP',
-      data: data.map(datum => [new Date(datum.date).getTime(), parseFloat(datum.value)])
-    }],
-    xAxis: {
-      type: 'datetime',
-      title: {
-        text: 'Year'
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Billions of Dollars'
-      }
-    }
-  };
-
+const HomePage = () => {
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <HighchartsPage />
     </div>
   );
 };
 
-export default HighchartsPage;
+export default HomePage;
