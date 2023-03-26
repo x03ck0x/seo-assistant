be9f6fffa6ec53efd19f39c7f8691847406769b2
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-26 16:38:46 +0200

    moor2

--- pages/img.js
+++ pages/img.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from 'react';
-import generateImage from './img-gen';
+import generateImage from './api/generate-img';
 
 const ImageGenerator = () => {
   useEffect(() => {
