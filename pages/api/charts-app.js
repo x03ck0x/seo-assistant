85a4512077df2dab2104d759a530f7d2aebd6a8c
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-27 04:05:24 +0200

    moor4

--- pages/api/charts-app.js
+++ pages/api/charts-app.js
@@ -3,10 +3,36 @@ import React, { useEffect, useState } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
-// ... (bloombergTheme, seriesOptions, etc.)
+const bloombergTheme = {
+  // ... (theme options)
+};
+
+const seriesOptions = [
+  { value: 'GNPCA', label: 'US Real Gross Domestic Product' },
+  // Add more series options here
+];
 
 const HighchartsPage = () => {
-  // ... (state variables, useEffect, fetchData, handleChangeSeries)
+  const [data, setData] = useState([]);
+  const [selectedSeries, setSelectedSeries] = useState(seriesOptions[0].value);
+
+  useEffect(() => {
+    fetchData(selectedSeries);
+  }, [selectedSeries]);
+
+  const fetchData = async (seriesId) => {
+    try {
+      const response = await fetch(`/api/data?series_id=${seriesId}`);
+      const json = await response.json();
+      setData(json.observations);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeSeries = (event) => {
+    setSelectedSeries(event.target.value);
+  };
 
   const options = {
     title: {
@@ -15,7 +41,7 @@ const HighchartsPage = () => {
     series: [
       {
         name: 'Series',
-        data: data && data.map(datum => [new Date(datum.date).getTime(), parseFloat(datum.value)]),
+        data: data ? data.map(datum => [new Date(datum.date).getTime(), parseFloat(datum.value)]) : [],
       },
     ],
     xAxis: {
@@ -33,9 +59,20 @@ const HighchartsPage = () => {
   };
 
   return (
-    // ... (JSX)
+    <div>
+      <div>
+        <label htmlFor="series-select">Select series:</label>
+        <select id="series-select" value={selectedSeries} onChange={handleChangeSeries}>
+          {seriesOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
   );
 };
 
 export default HighchartsPage;
-
