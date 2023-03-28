7656e60045ca55be4e88145561c20747cae83331
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-03-28 02:15:13 +0200

    moor4

--- pages/api/charts-app.js
+++ pages/api/charts-app.js
@@ -116,15 +116,6 @@ const HighchartsPage = () => {
     setSelectedSeries(event.target.value);
   };
 
-  useEffect(() => {
-    const chart = Highcharts.charts[HighchartsReact.charts.findIndex((c) => c && c.options === options)];
-    if (chart) {
-      chart.setTitle({
-        text: seriesOptions.find((option) => option.value === selectedSeries)?.label || '',
-      });
-    }
-  }, [selectedSeries, options]);
-  
   const options = {
     title: {
       text: seriesOptions.find((option) => option.value === selectedSeries)?.label || '',
@@ -168,6 +159,15 @@ const HighchartsPage = () => {
       },
     ...bloombergTheme,
   };
+  
+  useEffect(() => {
+    const chart = Highcharts.charts[HighchartsReact.charts.findIndex((c) => c && c.options === options)];
+    if (chart) {
+      chart.setTitle({
+        text: seriesOptions.find((option) => option.value === selectedSeries)?.label || '',
+      });
+    }
+  }, [selectedSeries, options]);
 
   return (
     <div>
