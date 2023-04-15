4409722362d3355581bd9e288cdbfc074033120a
x03ck0x <107725727+x03ck0x@users.noreply.github.com>
2023-04-15 16:12:27 +0000

    f

---
+++ pages/api/generate-predictoor.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+    const { user } = req.body;
+    const prompt = generatePrompt( user );
+
+    console.log(prompt);
+
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.8,
+      max_tokens: 200,
+    });
+
+    res.status(200).json({ result: completion.data.choices[0].text });  
+}
+
+function generatePrompt(user) {
+  
+  //return `Generate a high-quality Meta Description in for this keywords: ${keywords}$ , for the entreprise user use this ${user}$ , use ${services}$ as services they provide, use this address: ${address}$ , as address or city.`
+  return `
+  You are a sarcastic trader and u like to predict the stock prices and can answer everything!, u never ever say that u dont know, you always make up price foir the future based on the current
+  , answer in this context or the following text prompt :
+  - ${user}
+  `
+}
---
+++ pages/predictoor.js
@@ -0,0 +1,91 @@
+import react from "react";
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [user, setUser] = useState("Enter the company name or ticker");
+
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState();
+  
+  async function onSubmit(event) {
+    event.preventDefault();
+    
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+
+      const response = await fetch('/api/generate-predictoor', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user }),
+      });
+      const data = await response.json();
+      setResult(data.result.replaceAll('\n', '<br />'));
+
+    } catch (e) {
+      alert('failed to submit, maybe your a loser')
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    
+    <div className="container">
+      <Head>
+        <title>Test</title>
+        <link rel="icon" href="/skull.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/skull.png" className={styles.icon} />
+        <h3>Beta 0.1</h3>
+        <form onSubmit={onSubmit}>
+          <label>Text</label>
+          <input
+            type="text"
+            name="user"
+            placeholder="Enter request"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+         
+
+          <input type="submit" value="Generate" />          
+        </form>
+
+        {loading && (
+          <div>
+            <h4>Working...</h4>
+
+          </div>
+        )}
+        {result && (
+        <div className={styles.result2} 
+        dangerouslySetInnerHTML={{ __html: result }}
+        />
+        )}
+      </main>
+
+      <style jsx global>{`
+        body {
+          background-color: #212129;
+          color: #fff !important;
+        }
+        p, h1, h2, h3, h4, h5, h6 {
+          color: #fff !important;
+        }
+      `}</style>
+
+    </div>
+  );
+}
+
+
